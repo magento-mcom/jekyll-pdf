f6b6f9a439a18c8ca61d2abdb773f9ad4349a869
Philippe Bernou <pbernou@magento.com>
2016-10-14 13:16:18 +0200

    Changing structure to avoid endless loop

--- lib/jekyll/pdf/generator.rb
+++ lib/jekyll/pdf/generator.rb
@@ -5,12 +5,17 @@ module Jekyll
       priority :lowest
 
       def generate(site)
-        # Loop through pages & documents and build PDFs
-        [site.pages, site.documents].each do |items|
-          items.each do |item|
+
+          # Loop through pages & documents and build PDFs
+
+          pages_array = site.pages.clone
+
+          pages_array.each do |item|
+
             site.pages << Document.new(site, site.source, item) if item.data['pdf']
+
           end
-        end
+
       end
 
     end
